b3995610b2a601ab0b1df7f5c6f3d9cd71f32621
shubhambargal2002 <shubhambargal2002@gmail.com>
2023-02-07 11:29:26 +0530

    db.js changes

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
+app.get("/", (req, res) => {
+  res.send("API Running!");
+});
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
